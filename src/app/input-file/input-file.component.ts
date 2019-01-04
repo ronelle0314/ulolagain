d88fe63ce8b186f51f8611c6e6447fec345322c2
ronelle0314 <46138467+ronelle0314@users.noreply.github.com>
2019-01-04 16:08:56 +0800

    Add files via upload

---
+++ src/app/input-file/input-file.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { BillService } from '../shared/bill/bill.service';
+import { UploadFileService } from '../shared/upload/upload-file.service';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-input-file',
+  templateUrl: './input-file.component.html',
+  styleUrls: ['./input-file.component.css']
+})
+export class InputFileComponent implements OnInit {
+  
+  selectedFiles: FileList
+  currentFileUpload: File
+  progress: { percentage: number } = { percentage: 0 }
+  constructor(private uploadService: UploadFileService) { }
+
+  ngOnInit() {
+  }
+
+  selectFile(event) {
+    this.selectedFiles = event.target.files;
+    console.log(this.selectedFiles);
+  }
+
+
+  
+  upload() {
+    this.progress.percentage = 0;
+
+    this.currentFileUpload = this.selectedFiles.item(0)
+    this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress.percentage = Math.round(100 * event.loaded / event.total);
+      } else if (event instanceof HttpResponse) {
+        console.log('File is completely uploaded!');
+      }
+    })
+
+    this.selectedFiles = undefined
+  }
+  //@Output() sendFilePathToAppComponentEvent = new EventEmitter<string>();   
+  
+  // fileUploaded(e) {
+  //   this.sendFilePathToAppComponentEvent.emit(e.target.value);  
+  //   this.billService.postTestToApi(); 
+  // }
+}
